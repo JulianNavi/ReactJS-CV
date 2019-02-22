8a3934506695b6380fb65d6014346de0bbf43122
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2019-02-22 19:04:54 +0200

    finished footer style

--- src/app/index.js
+++ src/app/index.js
@@ -127,8 +127,20 @@ function App() {
         </main>
       </div>
       <footer className="Footer">
-        <FaLinkedinIn />
-        <FaGithub />
+        <a
+          href="https://www.linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          href="https://github.com/JulianNavi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
       </footer>
     </React.Fragment>
   );
